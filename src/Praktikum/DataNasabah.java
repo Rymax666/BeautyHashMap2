834fcde8e4b72c011a296b48b625c4b67bd45bbb
Rymax666 <rymax.xxx@gmail.com>
2019-12-18 21:10:42 +0700

    KEMEM

---
+++ src/Praktikum/DataNasabah.java
@@ -0,0 +1,19 @@
+package Praktikum;
+
+public class DataNasabah {
+    public int nope;
+    public String nama;
+
+    DataNasabah(String nama, int nope){
+        this.nama = nama;
+        this.nope = nope;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getNope() {
+        return nope;
+    }
+}
--- src/Praktikum/Main.java
+++ src/Praktikum/Main.java
@@ -6,39 +6,52 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String,String> data = new HashMap<>();
+        HashMap<String,DataNasabah> data = new HashMap<>();
         Scanner scan = new Scanner(System.in);
+        DataNasabah nasabah;
+        char jwb;
+        do {
+            System.out.println(">>Tambah Data<<");
+            System.out.print("Masukkan Nomor Rekening : ");
+            String norek = scan.next();
+            System.out.print("Masukkan Nama : ");
+            String nama = scan.next();
+            System.out.print("Masukkan Nomor HP : ");
+            int nope = scan.nextInt();
 
-        data.put("Bendy", "Lalapan Boy");
-        data.put("Comsans", "Papahe Ferro");
-        data.put("Asnawy", "Papahe Yanto");
-        data.put("Agos", "Papahe Cipong");
+            data.put(norek, new DataNasabah(nama, nope));
 
-        System.out.println(">>Input Dinamis<<");
-        System.out.print("Masukkan Nama : ");
-        String nama = scan.next();
-        System.out.print("Masukkan Identitas : ");
-        String identitas = scan.next();
+            System.out.print("Mau Lagi ? y/n : ");
+            jwb = scan.next().charAt(0);
+        }while (jwb == 'y' || jwb == 'Y');
 
-        data.put(nama,identitas);
-
-        System.out.println();
-        System.out.println(">>Mencari Key<<");
-        System.out.print("Masukkan Key : ");
+        System.out.println(">>Cari Nasabah<<");
+        System.out.print("Masukan Nomor Rekening : ");
         String key = scan.next();
 
-        if (data.containsKey(key)){
-            System.out.println("Identitas : "+data.get(key));
+        if (data.get(key) == null){
+            System.out.println("Data Tidak Ditemukan!");
         }else{
-            System.out.println("Kagak Ada Anjir!!!");
+            nasabah = data.get(key);
+            System.out.println("Nomor Rekening  : "+key);
+            System.out.println("Nama            : "+nasabah.getNama());
+            System.out.println("Nomor HP        : "+nasabah.getNope());
         }
 
-        System.out.println(">>Menampilkan Data<<");
-        System.out.println();
-        for (String orang : data.keySet()){
-            String Value = data.get(orang);
-            System.out.println("["+orang+", "+Value+"]");
+        System.out.print("Tampilkan Semua Data ? y/n : ");
+        char jjwb = scan.next().charAt(0);
+
+        if (jjwb == 'y' || jjwb == 'Y'){
+            System.out.println(">>Menampilkan Semua Data<<");
+            System.out.println();
+            for (String nsb : data.keySet()){
+                DataNasabah value = data.get(nsb);
+                System.out.println("[Nomor Rekening : "+nsb+", Nama : "+value.getNama()+", Nomor HP : "+value.getNope()+"]");
+            }
+        }else {
+            System.out.println("Byee");
         }
+
 
     }
 }
